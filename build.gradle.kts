7fb6343512b877768d17c97db83edfea99390fd5
MukjepScarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-09-19 13:58:39 +0800

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "moe.lasoleil"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    compileOnly("it.unimi.dsi:fastutil:8.5.15")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+    jvmToolchain(8)
+}
